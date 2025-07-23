8c40183bafc8266f08ad9203660b27eff0bc0593
nox-vobiscum <noxvobiscum@gmail.com>
2025-07-23 09:34:51 +0200

    first commit initial

---
+++ src/main/java/com/example/estpoker/EstpokerApplication.java
@@ -0,0 +1,13 @@
+package com.example.estpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstpokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstpokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/estpoker/EstpokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstpokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
